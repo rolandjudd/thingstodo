483d6e0877902a1c1bdee46160e749235779cd55
alex <apentlander@gmail.com>
2014-10-14 17:25:02 -0400

    Added starting point for backbone frontend

---
+++ frontend/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Frontend, $*/
+
+
+window.Frontend = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Frontend.init();
+});
