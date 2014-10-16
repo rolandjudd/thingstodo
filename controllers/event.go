63163442c8763472595616f72f28d6541d616559
Roland Judd <rolandjudd@gmail.com>
2014-10-16 13:09:52 -0400

    Update event controller

--- controllers/event.go
+++ controllers/event.go
@@ -2,13 +2,17 @@ package controllers
 
 import "github.com/codegangsta/martini-contrib/render"
 import "gopkg.in/mgo.v2"
+import "gopkg.in/mgo.v2/bson"
 import "thingstodo/models"
-import "encoding/json"
 
-func GetAllEvents(db *mgo.Database, r render.Render) string {
-	e := models.Event{}
-	e.SetTitle("Pub Crawl")
-	e.SetDescription("Testing Descripting")
-	bytes, _  := json.Marshal(e)
-	return string(bytes)
+func GetAllEvents(db *mgo.Database, r render.Render)  {
+
+	// Query All
+	var events []models.Event
+	err := db.C("events").Find(bson.M{}).All(&events)
+	if err != nil {
+		panic(err)
+	}
+
+	r.JSON(200, events)
 }
