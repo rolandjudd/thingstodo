abfec30afdd96b7b76f54e961700c0dbf4332566
Roland Judd <rolandjudd@gmail.com>
2014-10-16 13:10:53 -0400

    Update main.go

--- main.go
+++ main.go
@@ -8,22 +8,21 @@ import "thingstodo/db"
 
 func main() {
 
-    m := martini.Classic()
-    
-    // Setup middleware
-    //m.Use(martini.Recovery())
-    //m.Use(martini.Logger())
-    m.Use(db.DB())
-    m.Use(render.Renderer())
+	m := martini.Classic()
 
+	// Setup middleware
+	//m.Use(martini.Recovery())
+	//m.Use(martini.Logger())
+	m.Use(db.DB())
+	m.Use(render.Renderer())
 
-    // Setup routes
-    m.Get(`/event`, controllers.GetAllEvents)
-    //r.Get(`/event/:id`, GetEvent)
-    //r.Post(`/albums`, AddEvent)
-    //r.Put(`/albums/:id`, UpdateEvent)
-    //r.Delete(`/albums/:id`, DeleteEvent)
+	// Setup routes
+	m.Get(`/event`, controllers.GetAllEvents)
+	//r.Get(`/event/:id`, GetEvent)
+	//r.Post(`/albums`, AddEvent)
+	//r.Put(`/albums/:id`, UpdateEvent)
+	//r.Delete(`/albums/:id`, DeleteEvent)
 
-    // Add the router action
-    m.Run()
+	// Add the router action
+	m.Run()
 }
