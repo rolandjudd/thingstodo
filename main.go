10bfaa7c84961e1264e7aeb9c7c8f8cb4e4f16c1
Roland Judd <rolandjudd@gmail.com>
2014-10-15 21:24:28 -0400

    Added main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "github.com/go-martini/martini"
+import "github.com/codegangsta/martini-contrib/render"
+
+import "thingstodo/controllers"
+import "thingstodo/db"
+
+func main() {
+
+    m := martini.Classic()
+    
+    // Setup middleware
+    //m.Use(martini.Recovery())
+    //m.Use(martini.Logger())
+    m.Use(db.DB())
+    m.Use(render.Renderer())
+
+
+    // Setup routes
+    m.Get(`/event`, controllers.GetAllEvents)
+    //r.Get(`/event/:id`, GetEvent)
+    //r.Post(`/albums`, AddEvent)
+    //r.Put(`/albums/:id`, UpdateEvent)
+    //r.Delete(`/albums/:id`, DeleteEvent)
+
+    // Add the router action
+    m.Run()
+}
