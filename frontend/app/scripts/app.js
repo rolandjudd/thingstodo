52a17aff3f1deab8eecaf5a49bcba27d2294bbe1
alex <apentlander@gmail.com>
2014-10-21 21:54:02 -0400

    Replaced backbone files with react framework

---
+++ frontend/app/scripts/app.js
@@ -0,0 +1,125 @@
+/** @jsx React.DOM */
+
+var React = window.React = require('react'),
+    $ = require('jquery-browserify'),
+    mountNode = document.getElementById("app");
+
+var Event = React.createClass({
+    render: function () {
+        return (
+            <div className="event">
+                <h2 className="title">
+                    {this.props.title}
+                </h2>
+                {this.props.children}
+            </div>
+        )
+    }
+});
+
+var EventList = React.createClass({
+    render: function () {
+        var eventNodes = this.props.data.map(function(event, index) {
+            return (
+                <Event title={event.title} key={index}>
+                    {event.description}
+                </Event>
+            );
+        });
+        return (
+            <div className="eventList">
+                {eventNodes}
+            </div>
+        );
+    }
+});
+
+
+var EventForm = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault()
+        var title = this.refs.title.getDOMNode().value.trim();
+        var description = this.refs.description.getDOMNode().value.trim();
+        var latitude = this.refs.latitude.getDOMNode().value;
+        var longitude = this.refs.longitude.getDOMNode().value;
+        var coordinates = [parseFloat(latitude), parseFloat(longitude)];
+
+        if (!title || !description) {
+            return;
+        }
+        this.props.onEventSubmit({
+            title: title, 
+            description: description,
+            coordinates: coordinates
+        });
+        this.refs.title.getDOMNode().value = "";
+        this.refs.description.getDOMNode().value = "";
+        return;
+    },
+    render: function() {
+        return (
+            <form className="eventForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Event title" ref="title" />
+                <input type="text" placeholder="Event description" ref="description" />
+                <input type="number" placeholder="0.0" ref="latitude" />
+                <input type="number" placeholder="0.0" ref="longitude" />
+                <input type="submit" value="Post" />
+            </form>
+        )
+    }
+});
+
+
+var EventBox = React.createClass({
+    loadEventsFromServer: function () {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'GET',
+            success: function (data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    handleEventSubmit: function(event) {
+        var events = this.state.data;
+        events.push(event);
+        console.log(JSON.stringify(event));
+        this.setState({data: events}, function() {
+            $.ajax({
+                url: this.props.url,
+                dataType: 'json',
+                type: 'POST',
+                data: event,
+                success: function(data) {
+                    this.setState({data: data});
+                }.bind(this),
+                error: function (xhr, status, err) {
+                    console.error(this.props.url, status, err.toString());
+                }.bind(this)
+            });
+        });
+    },
+    getInitialState: function () {
+        return {data: []};
+    },
+    componentDidMount: function () {
+        this.loadEventsFromServer();
+        setInterval(this.loadEventsFromServer, this.props.pollInterval);
+    },
+    render: function () {
+        return (
+            <div className="eventBox">
+                <h1>Events</h1>
+                <EventList data={this.state.data} />
+                <EventForm onEventSubmit={this.handleEventSubmit} />
+            </div>
+        );
+    }
+});
+
+
+React.renderComponent(<EventBox url="http://localhost:3000/events" pollInterval={5000} />, mountNode);
---
+++ frontend/gulpfile.js
@@ -0,0 +1,104 @@
+'use strict';
+
+var gulp = require('gulp');
+var reactify = require('reactify');
+var del = require('del');
+var handleErrors = require('./util/handleErrors');
+
+
+// Load plugins
+var $ = require('gulp-load-plugins')();
+
+
+// Scripts
+gulp.task('scripts', function () {
+    return gulp.src('app/scripts/app.js')
+        .pipe($.browserify({
+            insertGlobals: true,
+            transform: ['reactify']
+        }))
+        .on('error', handleErrors)
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe($.size());
+    });
+
+
+// HTML
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size());
+});
+
+// Images
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe($.size());
+});
+
+
+// Clean
+gulp.task('clean', function (cb) {
+    del(['dist/styles', 'dist/scripts', 'dist/images'], cb);
+});
+
+
+// Bundle
+gulp.task('bundle', ['scripts', 'bower'], function(){
+    return gulp.src('./app/*.html')
+               .pipe($.useref.assets())
+               .pipe($.useref.restore())
+               .pipe($.useref())
+               .pipe(gulp.dest('dist'));
+});
+
+// Build
+gulp.task('build', ['html', 'bundle', 'images']);
+
+// Default task
+gulp.task('default', ['clean', 'build']);
+
+// Webserver
+gulp.task('serve', function () {
+    gulp.src('dist')
+        .pipe($.webserver({
+            livereload: true,
+            port: 9000
+        }));
+});
+
+// Bower helper
+gulp.task('bower', function() {
+    gulp.src('app/bower_components/**/*.js', {base: 'app/bower_components'})
+        .pipe(gulp.dest('dist/bower_components/'));
+
+});
+
+gulp.task('json', function() {
+    gulp.src('app/scripts/json/**/*.json', {base: 'app/scripts'})
+        .pipe(gulp.dest('dist/scripts/'));
+});
+
+
+// Watch
+gulp.task('watch', ['html', 'bundle', 'serve'], function () {
+
+    // Watch .json files
+    gulp.watch('app/scripts/**/*.json', ['json']);
+
+    // Watch .html files
+    gulp.watch('app/*.html', ['html']);
+
+    // Watch .js files
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+
+    // Watch image files
+    gulp.watch('app/images/**/*', ['images']);
+});
