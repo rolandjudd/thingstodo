04bd12e40eb1966a164954fd8b111385982e468b
Roland Judd <rolandjudd@gmail.com>
2014-10-27 18:16:40 -0400

    Use the frontend/dist directory for static assets

--- main.go
+++ main.go
@@ -24,6 +24,8 @@ func NewServer(databaseName string) *martini.ClassicMartini {
 		AllowCredentials: true,
 	}))
 
+	m.Use(martini.Static("frontend/dist"))
+	
 	// Setup routes
 	m.Get(`/events`, controllers.GetAllEvents)
 	m.Get(`/events/:id`, controllers.GetEvent)
