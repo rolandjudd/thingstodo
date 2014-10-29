5a4b859a05a39ae9c8eafc01bb7d4a24c08d1668
12mholmes <12mholmes@gmail.com>
2014-10-29 16:17:14 -0400

    Merging Backend testing

---
+++ controllers/controllers_suite_test.go
@@ -0,0 +1,50 @@
+package controllers_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+        "github.com/rolandjudd/thingstodo/models"    
+        "github.com/modocache/gory"
+        "testing"
+        "time"
+        "gopkg.in/mgo.v2/bson"
+)
+
+func TestControllers(t *testing.T) {
+    defineFactories()
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Controllers Suite")
+}
+
+func defineFactories() {
+    gory.Define("event", models.Event{},
+        func(factory gory.Factory) {
+            factory["Title"] = "Yard Sale!"
+            factory["Description"] = "Getting a bunch of old stuff out of the attic!"
+            factory["Category"] = "Sale"
+            factory["CreatedBy"] = bson.NewObjectId()
+            factory["StartTime"] = time.Date(2999, time.January, 1, 12, 12, 12, 12, time.UTC)
+            factory["EndTime"] = time.Date(2999, time.January, 2, 12, 12, 12, 12, time.UTC)
+        })
+
+    gory.Define("eventAlreadyOver", models.Event{},
+        func(factory gory.Factory) {
+            factory["Title"] = "Pubcrawl!"
+            factory["Description"] = "Come to the Ruck!"
+            factory["Category"] = "Pubcrawl"
+            factory["CreatedBy"] = bson.NewObjectId()
+            factory["StartTime"] = time.Now().UTC()
+            factory["EndTime"] = time.Now().UTC()
+        })
+        
+    gory.Define("eventTitleTooShort", models.Event{},
+        func(factory gory.Factory) {
+            factory["Title"] = "Test"
+            factory["Description"] = "Trying ThingsToDo"
+            factory["Category"] = "Other"
+            factory["CreatedBy"] = bson.NewObjectId()
+            factory["StartTime"] = time.Date(2999, time.January, 1, 12, 12, 12, 12, time.UTC)
+            factory["EndTime"] = time.Date(2999, time.January, 2, 12, 12, 12, 12, time.UTC)
+        })
+}
+
---
+++ server/server.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"github.com/codegangsta/martini-contrib/binding"
+	"github.com/codegangsta/martini-contrib/cors"
+	"github.com/codegangsta/martini-contrib/render"
+	"github.com/go-martini/martini"
+	"github.com/rolandjudd/thingstodo/controllers"
+	"github.com/rolandjudd/thingstodo/db"
+	"github.com/rolandjudd/thingstodo/models"
+)
+
+func NewServer(databaseName string) *martini.ClassicMartini {
+	m := martini.Classic()
+
+	// Setup middleware
+	m.Use(db.DB(databaseName))
+	m.Use(render.Renderer())
+	m.Use(cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"http://localhost*"},
+		AllowMethods:     []string{"POST", "GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	// Setup routes
+	m.Get(`/events`, controllers.GetAllEvents)
+	m.Get(`/events/:id`, controllers.GetEvent)
+	m.Post(`/events`, binding.Json(models.Event{}), binding.ErrorHandler, controllers.AddEvent)
+
+	// TODO Update, Delete for events
+	//m.Put(`/events/:id`, UpdateEvent)
+	//m.Delete(`/events/:id`, DeleteEvent)
+
+	// Add the router action
+    
+    return m
+}
