c15b914381856be6d2921ecc466522b70206c459
12mholmes <12mholmes@gmail.com>
2014-11-05 14:45:32 -0500

    Initial comment unit tests

--- controllers/controllers_suite_test.go
+++ controllers/controllers_suite_test.go
@@ -17,6 +17,8 @@ func TestControllers(t *testing.T) {
 }
 
 func defineFactories() {
+	validID := bson.NewObjectId()
+
 	gory.Define("event", models.Event{},
 		func(factory gory.Factory) {
 			factory["Title"] = "Yard Sale!"
@@ -46,4 +48,22 @@ func defineFactories() {
 			factory["StartTime"] = time.Date(2999, time.January, 1, 12, 12, 12, 12, time.UTC)
 			factory["EndTime"] = time.Date(2999, time.January, 2, 12, 12, 12, 12, time.UTC)
 		})
+
+	gory.Define("eventCommentTest", models.Event{},
+		func(factory gory.Factory) {
+			factory["Title"] = "Yard Sale!"
+			factory["Description"] = "Getting a bunch of old stuff out of the attic!"
+			factory["Category"] = "Sale"
+			factory["Id"] = validID
+			factory["CreatedBy"] = bson.NewObjectId()
+			factory["StartTime"] = time.Date(2999, time.January, 1, 12, 12, 12, 12, time.UTC)
+			factory["EndTime"] = time.Date(2999, time.January, 2, 12, 12, 12, 12, time.UTC)
+		})
+
+	gory.Define("comment", models.Comment{},
+		func(factory gory.Factory) {
+			factory["Comment"] = "This is a valid comment."
+			factory["EventId"] = validID
+		})
+
 }
