64e341fd36c6bebcf38273a2a9e919e73d364d15
alex <apentlander@gmail.com>
2014-11-20 17:06:33 -0500

    Comments are toggleable

--- frontend/app/scripts/Event.js
+++ frontend/app/scripts/Event.js
@@ -4,6 +4,18 @@ var React = window.React = require('react');
 
 // define the event
 var Event = React.createClass({
+    handleToggleComments: function () {
+        if (this.state.comments == true) {
+            this.setState({comments: false});
+        } else {
+            this.setState({comments: true});
+        }
+    },
+
+    getInitialState: function () {
+        return {comments: false};
+    },
+
     render: function () {
         // require CommentBox.js logic
         var CommentBox = require('./CommentBox');
@@ -17,10 +29,15 @@ var Event = React.createClass({
         // return dom 'event' element
         return (
             <div className="event">
-            {title}
-            {this.props.children}
-            <CommentBox eventId={this.props.id} url={url} />
-            <hr />
+                {title}
+                {this.props.children}
+                <br />
+                <br />
+                <button type="button" className="btn btn-default btn-xs" onClick={this.handleToggleComments}>
+                    Comments
+                </button>
+                {this.state.comments ? <CommentBox eventId={this.props.id} url={url} /> : null}
+                <hr />
             </div>
         );
     }
