65fe90c89124c22446cdf67fc8c5c4f52ec6ab21
Roland Judd <rolandjudd@gmail.com>
2014-11-20 13:02:37 -0500

    Remove imagemin from gulpfile

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -66,11 +66,6 @@ gulp.task('fonts', function () {
 // Images
 gulp.task('images', function () {
     return gulp.src('app/images/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
         .pipe(gulp.dest('dist/images'))
         .pipe($.size());
 });
